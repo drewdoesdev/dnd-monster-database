b7aaac0216aa47460de3374bb758326b066cd591
drewdoesdev <ataylor2247@gmail.com>
2021-10-07 11:17:33 -0500

    init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,12 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        accessToken: "CFPAT-hWO6GLNYME9m2CZksdEwmLJlDUyykktybzsegTzX-eo",
+        accessToken: "H1bJ1Qhm5c0nGF6UaqpnKr0r73igG-PVm3KDbsOq2qM",
         spaceId: "01qtuldtb10b",
       },
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
     },
     "gatsby-plugin-styled-components",
     "gatsby-plugin-sitemap",
